404b772d6d9ab85df635f4ca4413bce01e309a76
Elmir I-de <kryternext@gmail.com>
2018-08-10 02:38:34 +0400

    Added a first ViewSet and Router

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,8 +1,19 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
 app_name = 'profiles_api'
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
+
 urlpatterns = [
-    url(r'^hello-view/', views.HelloApiView.as_view())
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+    
+    # If django not found url, it start to find it in a router
+    url(r'', include(router.urls))
+
 ]
