4bd75ed3410f167ea20a9fbb092b825f1c0b05ee
Elmir I-de <kryternext@gmail.com>
2018-08-10 23:08:11 +0400

    Added ProfileFeedItem model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -60,3 +60,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    # model.CASCADE means if user deleted from system it deletes all related posts on account
+    user_profile = models.name = models.ForeignKey(
+        'UserProfile',
+        on_delete=models.CASCADE
+    )
+
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
