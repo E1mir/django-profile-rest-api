780174eb7c64ad491e08f921f9a02f88f0b07ebc
Elmir I-de <kryternext@gmail.com>
2018-08-11 01:31:04 +0400

    Added improved permissions to user profile feed items API

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -6,10 +6,22 @@ class UpdateOwnProfile(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile."""
-        
+
         # if request is not changing something like GET
         if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         # Check if the same user wants to change his profile
         return obj.id == request.user.id
+
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status."""
+
+    def has_object_permission(self, request, view, obj):
+        """Checks the user is trying to update their own status."""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -10,9 +10,11 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed', views.UserProfileFeedViewSet)
+
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
 
     # If django not found url, it start to find it in a router
-    url(r'', include(router.urls)),
+    url(r'', include(router.urls))
 ]
